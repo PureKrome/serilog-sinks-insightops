400cc8154b0ff18d314b6b6cfb260d98e8183da7
Pure Krome <github@world-domination.com.au>
2023-07-21 13:48:51 +1000

    Can set a custom formatter via config settings.

--- src/Serilog.Sinks.InsightOps/LoggerSinkConfigurationExtensions.cs
+++ src/Serilog.Sinks.InsightOps/LoggerSinkConfigurationExtensions.cs
@@ -12,12 +12,40 @@ namespace Serilog.Sinks.InsightOps
         const string DefaultOutputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}";
 
         /// <summary>
-        /// Writes events logs to insightOps.
+        /// Configuration file POCO class: writes events logs to insightOps.
         /// </summary>
         /// <param name="sinkConfiguration">Logger sink configuration.</param>
         /// <param name="token">Secret token which links these logs to your logset/account.</param>
         /// <param name="region">Region to send data to. User: au, eu, us, ca, jp.</param>
         /// <param name="useSsl">Use SSL or not. (SSl -might- have a slight performance hit, but don't quote me on that).</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for events passed through the sink. Ignored when levelSwitch is specified.</param>
+        /// <param name="formatter">The formatter to log events in a textual representation. E.G. a compact Json representation for structured logging.</param>
+        /// <param name="levelSwitch">A switch allowing the pass-through minimum level to be changed at runtime.</param>
+        /// <returns></returns>
+        public static LoggerConfiguration InsightOps(
+            this LoggerSinkConfiguration sinkConfiguration,
+            string token,
+            string region,
+            ITextFormatter formatter = null,
+            bool useSsl = true,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            LoggingLevelSwitch levelSwitch = null)
+        {
+            // We need a formatter, so lets use the other constructor.
+            if (formatter == null)
+            {
+                return InsightOps(sinkConfiguration, token, region, useSsl, restrictedToMinimumLevel, DefaultOutputTemplate, null, levelSwitch);
+            }
+
+            // We have a custom formatter, so we need to use the custom output template.
+            return InsightOps(sinkConfiguration, token, region, useSsl, formatter, restrictedToMinimumLevel, levelSwitch);
+        }
+
+        /// <summary>
+        /// Configuration file POCO class: writes events logs to insightOps.
+        /// </summary>
+        /// <param name="sinkConfiguration">Logger sink configuration.</param>
+        /// <param name="settings">Configuration settings.</param>
         /// <param name="restrictedToMinimumLevel">The minimum level for events passed through the sink. Ignored when levelSwitch is specified.</param>
         /// <param name="outputTemplate">Custom output template. If not provided, will default to <code>[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}</code></param>
         /// <param name="formatProvider">Optional: Supplies culture-specific formatting information, or null.</param>
@@ -31,42 +59,8 @@ namespace Serilog.Sinks.InsightOps
             LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
             string outputTemplate = DefaultOutputTemplate,
             IFormatProvider formatProvider = null,
-            LoggingLevelSwitch levelSwitch = null) => InsightOps(
-                sinkConfiguration,
-                new InsightOpsSinkSettings
-                {
-                    Token = token,
-                    Region = region,
-                    UseSsl = useSsl
-                },
-                restrictedToMinimumLevel,
-                outputTemplate,
-                formatProvider,
-                levelSwitch);
-
-        /// <summary>
-        /// Writes events logs to insightOps.
-        /// </summary>
-        /// <param name="sinkConfiguration">Logger sink configuration.</param>
-        /// <param name="config">Configuration settings.</param>
-        /// <param name="restrictedToMinimumLevel">The minimum level for events passed through the sink. Ignored when levelSwitch is specified.</param>
-        /// <param name="outputTemplate">Custom output template. If not provided, will default to <code>[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}</code></param>
-        /// <param name="formatProvider">Optional: Supplies culture-specific formatting information, or null.</param>
-        /// <param name="levelSwitch">A switch allowing the pass-through minimum level to be changed at runtime.</param>
-        /// <returns></returns>
-        public static LoggerConfiguration InsightOps(
-            this LoggerSinkConfiguration sinkConfiguration,
-            InsightOpsSinkSettings config,
-            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
-            string outputTemplate = DefaultOutputTemplate,
-            IFormatProvider formatProvider = null,
             LoggingLevelSwitch levelSwitch = null)
         {
-            if (config is null)
-            {
-                throw new ArgumentNullException(nameof(config));
-            }
-
             if (string.IsNullOrWhiteSpace(outputTemplate))
             {
                 throw new ArgumentException($"'{nameof(outputTemplate)}' cannot be null or whitespace", nameof(outputTemplate));
@@ -74,27 +68,40 @@ namespace Serilog.Sinks.InsightOps
 
             var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
 
-            return sinkConfiguration.InsightOps(config, formatter, restrictedToMinimumLevel, levelSwitch);
+            return sinkConfiguration.InsightOps(token, region, formatter, useSsl, restrictedToMinimumLevel, levelSwitch);
         }
 
+        /// <summary>
+        /// Configuration file POCO class: writes events logs to insightOps.
+        /// </summary>
+        /// <param name="sinkConfiguration">Logger sink configuration.</param>
+        /// <param name="settings">Configuration settings.</param>
+        /// <param name="formatter">The formatter to log events in a textual representation. E.G. a compact Json representation for structured logging.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for events passed through the sink. Ignored when levelSwitch is specified.</param>
+        /// <param name="levelSwitch">A switch allowing the pass-through minimum level to be changed at runtime.</param>
+        /// <returns></returns>
         public static LoggerConfiguration InsightOps(
             this LoggerSinkConfiguration sinkConfiguration,
-            InsightOpsSinkSettings config,
-            ITextFormatter formatter,
+            string token,
+            string region,
+            bool useSsl = true,
+            ITextFormatter formatter = null,
             LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
             LoggingLevelSwitch levelSwitch = null)
         {
-            if (config is null)
+            var settings = new InsightOpsSinkSettings
             {
-                throw new ArgumentNullException(nameof(config));
-            }
+                Token = token,
+                Region = region,
+                UseSsl = useSsl
+            };
 
             if (formatter is null)
             {
                 throw new ArgumentNullException(nameof(formatter));
             }
 
-            var sink = new InsightOpsSink(config, formatter);
+            var sink = new InsightOpsSink(settings, formatter);
 
             return sinkConfiguration.Sink(sink, restrictedToMinimumLevel, levelSwitch);
         }
